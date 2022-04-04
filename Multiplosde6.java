d39f3f40d32be7861cd5d91ff624096fd61eba78
joaofaggion17 <joaofaggion17@gmail.com>
2022-04-04 17:17:58 +0000

    Salvando

---
+++ Multiplosde6.java
@@ -0,0 +1,10 @@
+public class Multiplosde6{
+    public static void main(String[] args) {
+
+        for (int index = 0; index < 100; index++) {
+            if (index % 6 == 0) {
+                System.out.println("Números múltiplos de 6 entre 0 e 100: " + index);
+            }
+        }
+    }
+}
